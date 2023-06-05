eaf297b9657d0747c8e32643328309275b570946
ranumba <roanumba@gmail.com>
2023-06-05 14:10:06 -0700

    Partial checkin for statemangement.

--- src/context/TestModel.tsx
+++ src/context/TestModel.tsx
@@ -1,26 +1,9 @@
 
 import React, { useContext } from "react";
 import { GlobalContext } from "./GlobalContext";
+//create an abstract  class to manage  TestModel
 
-class TestModel {
-    get test() {
-        return this._test;
-    }
-    set test(value) {
-        const old = this._test;
-        this._test = value;
-        this.dispatch(old,value);
-    }
-    get cnt() {
-        return this._cnt;
-    }
-
-    set cnt(value) {
-        const old = this._cnt;
-        this._cnt = value;
-        this.dispatch(old,value);
-    }
-
+abstract class BaseMode {
     subscribe=(listener:(st:any)=>void) =>{
         const fn=listener.toString().replace(/\s/g,'');
         const lstns=this.listeners.find((l:any)=>l.toString().replace(/\s/g,'')===fn);
@@ -39,12 +22,73 @@ class TestModel {
             listener(this);
         });
     }
+    private listeners: any[] = [];
+
+}
+
+class TestModel extends BaseMode{
+    get test() {
+        return this._test;
+    }
+    set test(value) {
+        const old = this._test;
+        this._test = value;
+        this.dispatch(old,value);
+    }
+    get cnt() {
+        return this._cnt;
+    }
+
+    set cnt(value) {
+        const old = this._cnt;
+        this._cnt = value;
+        this.dispatch(old,value);
+    }
+
+
 
     private _test: string = 'test';
     private _cnt: number = 10;
-    private listeners: any[] = [];
 
 };
+
+//dynamically create a clases like TestModel
+
+function createModel(name:string) {
+    return class extends BaseMode{
+        get test() {
+            return this._test;
+        }
+        set test(value) {
+            const old = this._test;
+            this._test = value;
+            this.dispatch(old,value);
+        }
+        get cnt() {
+            return this._cnt;
+        }
+        set cnt(value) {
+            const old = this._cnt;
+            this._cnt = value;
+            this.dispatch(old,value);
+        }
+
+
+
+        private _test: string = 'test';
+        private _cnt: number = 10;
+
+    };
+}
+interface MyInterface {
+    property1: string;
+    property2: number;
+    }
+
+// Dynamically generate a class with get/set from the interface MyInterface
+
+
+//create a model from the interface
 
 function useTestModel() {
     const ctx = useContext(GlobalContext);
